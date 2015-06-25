ff4a7d5e66b361c80e64c3f2886804f95f18d83d
Philéas <phileas.condemine@ensae.fr>
2015-06-25 15:56:09 +0200

    Update route.R

--- R/route.R
+++ R/route.R
@@ -80,7 +80,7 @@ route<-function (from, to,through, mode = c("driving", "walking", "bicycling"),
   destination <- paste("destination=", destination, sep = "")
   waypoints <- through
   waypoints <- gsub(" ", "+", waypoints)
-  waypoints <- paste("waypoints=", waypoints, sep = "|",collapse='')
+  waypoints <- paste("waypoints=",paste(waypoints, sep = "|",collapse=''),sep="")
   
   mode4url <- paste("mode=", mode, sep = "")
   unit4url <- paste("units=", "metric", sep = "")
